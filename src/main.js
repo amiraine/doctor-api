706cf5ecda8e7f60a12f9e28b1c1050664c78fd8
ami cooper <amiraine.cooper@gmail.com>
2018-04-27 15:40:10 -0700

    add more styling/html through main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { API } from './../src/API.js';
+import './styles.css';
+
 //front end
 $(document).ready(function(){
   $('.search').submit(function(event){
@@ -18,7 +20,9 @@ $(document).ready(function(){
     issueInput = $('#issue').val();
     nameInput = $('#name').val();
     //functions on functions on functions
-    if (issueInput != "" && nameInput === ""){
+    if(issueInput === "" && nameInput === ""){
+      alert('You must fill in at least one of the search fields to continue');
+    } else if (issueInput != "" && nameInput === ""){
       issueAPI = newCall.issueSearch(issueInput);
       issueAPI.then(function(response){
         let doctorArrayLength;
@@ -26,18 +30,29 @@ $(document).ready(function(){
         doctorArrayLength = response.data.length;
         if(response.meta.total != 0){
           for(i=0; i < doctorArrayLength; i++){
-            $(".results").append(`<h3>`+ response.data[i].practices[0].name + `</h3><h5>`+ response.data[i].practices[0].visit_address.street+'<br>' + response.data[i].practices[0].visit_address.city +', ' +response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip +`</h5><p>Phone: `+ response.data[i].practices[0].phones[0].number);
+            $(".results").append(`<div class="card result-card">
+              <div class="card-header">
+                <h3>` + response.data[i].practices[0].name + `</h3>
+              </div>
+              <div class="card-body">
+                <h5>`+ response.data[i].practices[0].visit_address.street+'<br>' + response.data[i].practices[0].visit_address.city +', ' +response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip +`</h5><p>Phone: `+ response.data[i].practices[0].phones[0].number +
+              `</div>
+            </div>`
+              );
             if(response.data[i].practices[0].accepts_new_patients === true){
-              $(".results").append("<h4>"+"This doctor is currently accepting new patients!"+"</h4>")
+              $(".results").append("<div class='alert alert-success'>"+"This doctor is currently accepting new patients!"+"</div>")
             } else {
-              $(".results").append("<h4>"+"This doctor is not currently accepting new patients."+"</h4>")
+              $(".results").append("<div class='alert alert-warning'>"+"This doctor is not currently accepting new patients."+"</div>")
+            }
+            if(response.data[i].practices[0].website != undefined){
+              $(".results").append('<h2><a href ="'+response.data[i].practices[0].website+'">Website</a></h2>')
             }
           }
         } else {
           $(".results").html('<div class="alert alert-danger">No results matched your criteria. Please check your spelling or try another search</div>')
         }
       }).fail(function(error){
-        $(".results").text(`There was an error processing your request: ${error.responseText}`);
+        $(".results").html(`<div class="alert alert-danger">There was an error processing your request: ${error.responseText}</div>`);
       });
 ////// Search function for doctor name only /////
     } else if (issueInput === "" && nameInput != "") {
@@ -48,13 +63,28 @@ $(document).ready(function(){
         doctorNameArrayLength = response.data.length;
         if(response.meta.total !=0){
           for(j=0; j<doctorNameArrayLength; j++){
-            $(".results").append(`<h3>`+ response.data[j].practices[0].name + `</h3><h5>`+ response.data[j].practices[0].visit_address.street+'<br>' + response.data[j].practices[0].visit_address.city +', ' +response.data[j].practices[0].visit_address.state + " " + response.data[j].practices[0].visit_address.zip +`</h5><p>Phone: ` + response.data[j].practices[0].phones[0].number);
+            $(".results").append(`<div class="card result-card">
+              <div class="card-header"><h3>`+ response.data[j].practices[0].name + `</h3><h5>
+              </div>
+              <div class="card-body">`
+                + response.data[j].practices[0].visit_address.street+'<br>' + response.data[j].practices[0].visit_address.city +', ' +response.data[j].practices[0].visit_address.state + " " + response.data[j].practices[0].visit_address.zip +`</h5><p>Phone: ` + response.data[j].practices[0].phones[0].number +
+              `</div>
+            </div>`
+          );
+            if(response.data[j].practices[0].accepts_new_patients === true){
+              $(".results").append("<div class='alert alert-success'>"+"This doctor is currently accepting new patients!"+"</div>")
+            } else {
+              $(".results").append("<div class='alert alert-warning'>"+"This doctor is not currently accepting new patients."+"</div>")
+            }
+          }
+          if(response.data[j].practices[0].website != undefined){
+            $(".results").append('<h2><a href ="'+response.data[j].practices[0].website+'">Website</a></h2>')
           }
         } else {
           $(".results").html('<div class="alert alert-danger">No results matched your criteria. Please check your spelling or try another search</div>')
         }
       }).fail(function(error){
-        $(".results").text(`There was an error processing your request: ${error.responseText}`);
+        $(".results").html(`<div class="alert alert-danger">There was an error processing your request: ${error.responseText}</div>`);
       });
     } else if (issueInput != "" && nameInput != ""){
       hybridAPI = newCall.hybridSearch(issueInput,nameInput);
@@ -64,13 +94,28 @@ $(document).ready(function(){
         doctorHybrid = response.data.lengh;
         if(response.meta.total !=0){
           for(k=0;k<doctorHybrid;k++){
-            $(".results").append(`<h3>`+ response.data[k].practices[0].name + `</h3><h5>`+ response.data[k].practices[0].visit_address.street+'<br>' + response.data[k].practices[0].visit_address.city +', ' +response.data[k].practices[0].visit_address.state + " " + response.data[k].practices[0].visit_address.zip +`</h5><p>Phone: ` + response.data[k].practices[0].phones[0].number)
+            $(".results").append(`<div class="card result-card">
+              <div class="card-header"><h3>`+ response.data[k].practices[0].name + `</h3><h5>
+              </div>
+              <div class="card-body">`
+                + response.data[k].practices[0].visit_address.street+'<br>' + response.data[k].practices[0].visit_address.city +', ' +response.data[k].practices[0].visit_address.state + " " + response.data[k].practices[0].visit_address.zip +`</h5><p>Phone: ` + response.data[k].practices[0].phones[0].number +
+              `</div>
+            </div>`
+          );
+            if(response.data[k].practices[0].accepts_new_patients === true){
+              $(".results").append("<div class='alert alert-success'>"+"This doctor is currently accepting new patients!"+"</div>")
+            } else {
+              $(".results").append("<div class='alert alert-warning'>"+"This doctor is not currently accepting new patients."+"</div>")
+            }
+          }
+          if(response.data[k].practices[0].website != undefined){
+            $(".results").append('<h2><a href ="'+response.data[k].practices[0].website+'">Website</a></h2>')
           }
         } else {
           $(".results").html('<div class="alert alert-danger">No results matched your criteria. Please check your spelling or try another search</div>')
         }
       }).fail(function(error){
-        $(".results").text(`There was an error processing your request: ${error.responseText}`);
+        $(".results").html(`<div class="alert alert-danger"> There was an error processing your request: ${error.responseText} </div>`);
       });
     }
   });
