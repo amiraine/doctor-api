a436bbc34cc61b3c967eab9b71c5e6251d12ee5c
ami cooper <amiraine.cooper@gmail.com>
2018-04-27 10:00:37 -0700

    write out main.js, API calls not working?

--- src/API.js
+++ src/API.js
@@ -3,15 +3,16 @@ import $ from 'jquery'
 
 class API{
   issueSearch(issue){
-    let issueOutput = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=97202&skip=0&user_key=28ff22d61cad7e206962e721ab7ed101`)
-    return issueOutput
+    let issueOutput = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=45.523,-122.676,100&skip=2&limit=10&user_key=28ff22d61cad7e206962e721ab7ed101`);
+    return issueOutput;
   }
   nameSearch(name){
-    let nameOutput = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=97202&skip=0&user_key=28ff22d61cad7e206962e721ab7ed101`)
-    return nameOutput
+    let nameOutput = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.523,-122.676,100&skip=2&limit=10&user_key=28ff22d61cad7e206962e721ab7ed101`);
+    return nameOutput;
   }
-  hybridSearch(name,issue){
-    let hybridOutput = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=97202&skip=0&user_key=28ff22d61cad7e206962e721ab7ed101`)
+  hybridSearch(issue,name){
+    let hybridOutput = $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${issue}&location=45.523,-122.676,100&skip=2&limit=10&user_key=28ff22d61cad7e206962e721ab7ed101`);
+    return hybridOutput;
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,33 @@
-
+//import necessary files and classes
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { API } from './../src/API.js';
+//front end
 $(document).ready(function(){
-
+  $('.search').submit(function(event){
+    event.preventDefault();
+    //variables for search
+    let newCall;
+    let issueInput;
+    let nameInput;
+    let issueAPI;
+    let nameAPI;
+    let hybridAPI;
+    newCall = new API();
+    issueInput = $('#issue').val();
+    nameInput = $('#name').val();
+    //functions on functions on functions
+    if (issueInput != "" && nameInput === ""){
+      issueAPI = newCall.issueSearch(issueInput);
+      issueAPI.then(function(response){
+        $(".results").text(response.data[0].practices.name);
+      })
+    } else if (issueInput === "" && nameInput != "") {
+      nameAPI = newCall.nameSearch(nameInput)
+    } else if (issueInput != "" && nameInput != ""){
+      hybridAPI = newCall.hybridSearch(issueInput,nameInput);
+    }
+  });
 });
+// var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=45.523,-122.676,100&skip=2&limit=10&user_key=' + api_key;
