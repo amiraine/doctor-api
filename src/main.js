42df37832d86206a05c687d4eee7a5765bb1efc7
ami cooper <amiraine.cooper@gmail.com>
2018-04-27 16:15:07 -0700

    add additional styles, try to get the appended links into the doctor card bodies

--- src/main.js
+++ src/main.js
@@ -30,22 +30,23 @@ $(document).ready(function(){
         doctorArrayLength = response.data.length;
         if(response.meta.total != 0){
           for(i=0; i < doctorArrayLength; i++){
-            $(".results").append(`<div class="card result-card">
+            $(".results").append(`<div class="card result-card col-md-4">
               <div class="card-header">
                 <h3>` + response.data[i].practices[0].name + `</h3>
               </div>
               <div class="card-body">
                 <h5>`+ response.data[i].practices[0].visit_address.street+'<br>' + response.data[i].practices[0].visit_address.city +', ' +response.data[i].practices[0].visit_address.state + " " + response.data[i].practices[0].visit_address.zip +`</h5><p>Phone: `+ response.data[i].practices[0].phones[0].number +
-              `</div>
-            </div>`
+              `</div>`
               );
             if(response.data[i].practices[0].accepts_new_patients === true){
-              $(".results").append("<div class='alert alert-success'>"+"This doctor is currently accepting new patients!"+"</div>")
+              $(".results").append("<div class='alert alert-success'>"+"This doctor is currently accepting new patients!"+"</div></div>")
             } else {
               $(".results").append("<div class='alert alert-warning'>"+"This doctor is not currently accepting new patients."+"</div>")
             }
             if(response.data[i].practices[0].website != undefined){
-              $(".results").append('<h2><a href ="'+response.data[i].practices[0].website+'">Website</a></h2>')
+              $(".results").append('<h4><a href ="'+response.data[i].practices[0].website+'">'+response.data[i].practices[0].name+'</a></h4></div')
+            } else{
+              $(".results").append('</div>')
             }
           }
         } else {
@@ -63,22 +64,23 @@ $(document).ready(function(){
         doctorNameArrayLength = response.data.length;
         if(response.meta.total !=0){
           for(j=0; j<doctorNameArrayLength; j++){
-            $(".results").append(`<div class="card result-card">
+            $(".results").append(`<div class="card result-card col-md-4">
               <div class="card-header"><h3>`+ response.data[j].practices[0].name + `</h3><h5>
               </div>
               <div class="card-body">`
                 + response.data[j].practices[0].visit_address.street+'<br>' + response.data[j].practices[0].visit_address.city +', ' +response.data[j].practices[0].visit_address.state + " " + response.data[j].practices[0].visit_address.zip +`</h5><p>Phone: ` + response.data[j].practices[0].phones[0].number +
-              `</div>
-            </div>`
+              `</div>`
           );
             if(response.data[j].practices[0].accepts_new_patients === true){
-              $(".results").append("<div class='alert alert-success'>"+"This doctor is currently accepting new patients!"+"</div>")
+              $(".results").append("<div class='alert alert-success'>"+"This doctor is currently accepting new patients!"+"</div></div>")
             } else {
-              $(".results").append("<div class='alert alert-warning'>"+"This doctor is not currently accepting new patients."+"</div>")
+              $(".results").append("<div class='alert alert-warning'>"+"This doctor is not currently accepting new patients."+"</div></div>")
             }
           }
           if(response.data[j].practices[0].website != undefined){
-            $(".results").append('<h2><a href ="'+response.data[j].practices[0].website+'">Website</a></h2>')
+            $(".results").append('<h4><a href ="'+response.data[j].practices[0].website+'">'+response.data[j].practices[0].name+'</a></h4></div>')
+          } else {
+            $(".results").append('</div>');
           }
         } else {
           $(".results").html('<div class="alert alert-danger">No results matched your criteria. Please check your spelling or try another search</div>')
@@ -94,22 +96,23 @@ $(document).ready(function(){
         doctorHybrid = response.data.lengh;
         if(response.meta.total !=0){
           for(k=0;k<doctorHybrid;k++){
-            $(".results").append(`<div class="card result-card">
+            $(".results").append(`<div class="card result-card col-md-4">
               <div class="card-header"><h3>`+ response.data[k].practices[0].name + `</h3><h5>
               </div>
               <div class="card-body">`
                 + response.data[k].practices[0].visit_address.street+'<br>' + response.data[k].practices[0].visit_address.city +', ' +response.data[k].practices[0].visit_address.state + " " + response.data[k].practices[0].visit_address.zip +`</h5><p>Phone: ` + response.data[k].practices[0].phones[0].number +
-              `</div>
-            </div>`
+              `</div>`
           );
             if(response.data[k].practices[0].accepts_new_patients === true){
-              $(".results").append("<div class='alert alert-success'>"+"This doctor is currently accepting new patients!"+"</div>")
+              $(".results").append("<div class='alert alert-success'>"+"This doctor is currently accepting new patients!"+"</div></div>")
             } else {
-              $(".results").append("<div class='alert alert-warning'>"+"This doctor is not currently accepting new patients."+"</div>")
+              $(".results").append("<div class='alert alert-warning'>"+"This doctor is not currently accepting new patients."+"</div></div>")
             }
           }
           if(response.data[k].practices[0].website != undefined){
-            $(".results").append('<h2><a href ="'+response.data[k].practices[0].website+'">Website</a></h2>')
+            $(".results").append('<h4><a href ="'+response.data[k].practices[0].website+'">'+response.data[k].practices[0].name+'</a></h4></div>')
+          } else {
+            $('.results').append('</div>')
           }
         } else {
           $(".results").html('<div class="alert alert-danger">No results matched your criteria. Please check your spelling or try another search</div>')
